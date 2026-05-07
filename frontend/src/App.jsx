df208d6d87d144d5bea76a303313e0c3cb51f2d3
brian <brianyskeung@gmail.com>
2026-05-07 16:36:08 -0700

    Disabled highlighting of type display test and cursor changing for input bar. Removed clicking into input bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,24 @@
 import { useState } from 'react'
 import useTypingEngine from './hooks/useTypingEngine';
 import TypingDisplay from './components/TypingDisplay';
+import { useRef, useEffect } from 'react'
+
 
 function App() {
+    const inputRef = useRef(null)
     const { targetWords, currentWordIndex, typedWord, completedWords, handleKeyDown } = useTypingEngine()
+
+    useEffect(() => {
+        inputRef.current.focus()
+    }, [])
 
     console.log({ currentWordIndex, typedWord, completedWords })
 
     return (
-        <div className="min-h-screen w-full bg-[#323437] flex items-center justify-center">
+        <div onClick={() => inputRef.current.focus()} className="min-h-screen w-full bg-[#323437] flex items-center justify-center">
             <TypingDisplay targetWords={targetWords} currentWordIndex={currentWordIndex}
                 typedWord={typedWord} completedWords={completedWords} />
-            <input value={typedWord} onKeyDown={handleKeyDown} className="opacity-0 absolute" />
+            <input ref={inputRef} onKeyDown={handleKeyDown} className="opacity-0 absolute cursor-default" />
 
         </div>
     )
