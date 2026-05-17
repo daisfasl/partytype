4aa8a4848756ef5c2c90d34a4b8f19ead33848c7
Jacob Tan <jacobt07@students.cs.ubc.ca>
2026-05-18 01:48:41 +0800

    added more payloads

--- backend/app/schemas/payloads.py
+++ backend/app/schemas/payloads.py
@@ -2,18 +2,24 @@ from pydantic import BaseModel
 from typing import Literal
 
 class Payload(BaseModel):
-    pass
+    type: str
 
 class ProgressPayload(Payload):
+    type: Literal["progress"]
     cursor: int
     completed_words: int 
     wpm: float
-
-class ReadyPayload(Payload):
-    ready: bool
     
 class RoomPayload(Payload):
+    type: Literal["room"]
     status: Literal["waiting", "countdown", "active", "completed"]
     text: str
+    host: str
 
+class CountdownPayload(Payload):
+    pass 
 
+class MessagePayload(Payload):
+    type: Literal["message"]
+    sender: Literal["user", "server"]
+    message: str
