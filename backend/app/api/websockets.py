8d3b879d8c5048d785c673d2f4c11a2dec082a54
Jacob Tan <jacobt07@students.cs.ubc.ca>
2026-05-19 14:46:13 +0800

    adjusted to take in payloads

--- backend/app/api/websockets.py
+++ backend/app/api/websockets.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect
 from app.game.manager import manager    
+from pydantic import ValidationError, TypeAdapter
+from app.schemas.payloads import *
 import json
 
 router = APIRouter()
@@ -9,22 +11,31 @@ async def websocket_endpoint(ws: WebSocket, room_id: str, player_id: str):
     # connect to room
     await manager.connect(ws, room_id, player_id) 
     # broadcast to other players in the room that this player has joined
-    await manager.broadcast(room_id, {
-        "message": f"Player {player_id} has joined the party"
-    })
+    await manager.broadcast(room_id, MessagePayload(type = "message", 
+                                                    sender = "server", 
+                                                    message = f"Player {player_id} has joined the party"))
 
-    # listens for typing progress and other data from user 
+    # listens for payloads from user
     try: 
         while True:
             data = await ws.receive_text()
-            payload = json.loads(data)
-            payload["player_id"] = player_id
-            await manager.broadcast(room_id, payload)
+            payload = json.loads(data) # parse json
+            try:
+                payload = Payload.model_validate(payload) # checks if payload is valid (pydantic)
+            except ValidationError as error:
+                await ws.send_json({"message" : "error"})
+                return
+            match payload: # call corresponding ConnectionManager func. to payload
+                case RoomPayload():
+                    pass
+                case ProgressPayload():
+                    pass
 
     except WebSocketDisconnect:
         manager.disconnect(ws, room_id, player_id)
-        await manager.broadcast(room_id, {
-            "message" : "Player {player_id} disconnected..."
-        })
+        await manager.broadcast(room_id, MessagePayload(type = "message", 
+                                                    sender = "server",
+                                                    message = f"Player {player_id} has disconnected..."))
+
 
     
