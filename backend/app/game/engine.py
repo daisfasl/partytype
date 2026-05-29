2d363f2945503bc2cf6f343b9a2f55b173d45383
Jacob Tan <jacobt07@students.cs.ubc.ca>
2026-05-29 22:14:47 +0800

    added additional test for payload validation

--- backend/app/game/engine.py
+++ backend/app/game/engine.py
@@ -1,2 +1,11 @@
-## game engine ##
+import asyncio
+import time 
+from backend.app.game.manager import manager
 
+def calculate_wpm():
+    pass
+
+async def run_game(room: str, time_setting: int):
+    # starts game timer
+    await asyncio.sleep(time_setting)
+    
--- backend/tests/test_ws.py
+++ backend/tests/test_ws.py
@@ -1,5 +1,9 @@
 from fastapi.testclient import TestClient
-from app.main import app
+from backend.app.main import app
+from backend.app.schemas.payloads import *
+from pydantic import TypeAdapter
+import pytest
+import json
 
 tc = TestClient(app)
 
@@ -8,3 +12,14 @@ def test_ws():
         ws.send_text('{"type":"join","payload":{}}')
         msg = ws.receive_text()
         assert msg
+
+def test_payloads():
+   payload1 = {"type":"start",
+               "player_id":"test"}
+   payload1 = TypeAdapter(Payload).validate_python(payload1)
+   match payload1:
+       case StartPayload():
+           pass
+       case _:
+           pytest.fail()
+        
