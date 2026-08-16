30f9afab7e7412b223646aceb8298fddc4b28db0
brian <brianyskeung@gmail.com>
2026-08-15 18:50:52 -0700

    Basic temporary navigation setup complete

--- tui/src/App.tsx
+++ tui/src/App.tsx
@@ -2,14 +2,18 @@ import { useState } from 'react';
 import Home from './screens/Home.js';
 import Practice from './screens/Practice.js';
 
+type Screen = 'home' | 'practice';
 
 export default function App() {
-	const [currentScreen, setScreen] = useState('home');
+	const [currentScreen, setScreen] = useState<Screen>('home');
 
+	function navigateTo(screen: Screen) {
+		setScreen(screen);
+	}
 
 	if (currentScreen === 'home') {
-		return (<Home />)
-	} else if (currentScreen == 'practice') {
+		return (<Home onNavigate = {navigateTo} />)
+	} else if (currentScreen === 'practice') {
 		return (<Practice />)
 	}
 } 
--- tui/src/screens/Home.tsx
+++ tui/src/screens/Home.tsx
@@ -1,8 +1,17 @@
-import {Text} from 'ink';
+import { Text, useInput } from 'ink';
 
+interface HomeProps {
+	onNavigate: (screen: 'home' | 'practice') => void;
+}
 
-export default function Home() {
-	return(
-	<Text>Home test</Text>
+export default function Home({ onNavigate }: HomeProps) {
+	useInput((input, key) => {
+		if (input === 'p') {
+			onNavigate('practice');
+		}
+	});
+	
+	return (
+		<Text>Home test</Text>
 	);
 }
