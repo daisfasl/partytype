19ddcb2ce5fba74135bcbb54c122ea886960c5f5
Jacob Tan <jacobtan07@gmail.com>
2026-08-27 18:22:09 -0700

    refactored gameloop methods

--- backend/app/api/websockets.py
+++ backend/app/api/websockets.py
@@ -7,11 +7,7 @@ import json
 
 websockets_router = APIRouter()
 
-@websockets_router.websocket("/ws/party/{room_id}/{player_id}")
-async def websocket_endpoint(ws: WebSocket, room_id: str, player_id: str):
-    # connect to room
-    await manager.connect(ws, room_id, player_id) 
-    # broadcast to other players in the room that this player has joined
+async def game_loop(ws: WebSocket, player_id: str, room_id: str):
     await manager.broadcast(room_id, MessagePayload(type = "message", 
                                                     sender = "server", 
                                                     message = f"Player {player_id} has joined the party"))
@@ -25,8 +21,10 @@ async def websocket_endpoint(ws: WebSocket, room_id: str, player_id: str):
             type_adapter = TypeAdapter(Payload)
             try:
                 payload = type_adapter.validate_python(payload)
-            except ValidationError:
-                pass # continue looping :p
+            except ValidationError as e:
+                error_msg = ErrorPayload(type="error",
+                                         message = str(e))
+                await ws.send_json(error_msg.model_dump_json())
             match payload: # call corresponding ConnectionManager func. to payload
                 case StartPayload():
                     asyncio.create_task(manager.handle_host_start(room_id, payload))
@@ -35,8 +33,25 @@ async def websocket_endpoint(ws: WebSocket, room_id: str, player_id: str):
                 case MessagePayload():
                     await manager.broadcast(room_id, payload)
                 case FinishPayload():
-                    pass
+                    await manager.handle_player_finish(room_id, player_id)
     except WebSocketDisconnect:
         await manager.disconnect(ws, room_id, player_id)
+
+@websockets_router.websocket("/ws/party/create")
+async def websocket_create_party(ws: WebSocket, player_id: str):
+    room_id = await manager.create_party(ws, player_id)
+    await game_loop(ws, player_id, room_id)
+
+@websockets_router.websocket("/ws/party/{room_id}/{player_id}")
+async def websocket_endpoint(ws: WebSocket, player_id: str, room_id: str):
+    # connect to room
+    connection = await manager.connect(ws, room_id, player_id)
+    if connection: # connection = ErrorPayload
+       await ws.send_json(connection.model_dump_json()) # send error message to player
+       await ws.close()
+       return
+    else:
+        await game_loop(ws, player_id, room_id)
+
 
     
