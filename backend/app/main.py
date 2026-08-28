5a0e9cdaf0dad407631955fbdb81011eb4fd6fdf
brian <brianyskeung@gmail.com>
2026-08-27 18:04:46 -0700

    Added random word api endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,31 @@
 from fastapi import FastAPI
-from fastapi import WebSocket, WebSocketDisconnect
-from app.game.manager import manager 
-from app.api.websockets import router
+# from fastapi import WebSocket, WebSocketDisconnect
+from pathlib import Path
+import random
 import json
+
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "backend for party type :p"}
+# @app.get("/")
+# async def root():
+#     return {"message": "backend for party type :p"}
 
-app.include_router(router)
+@app.get("/api/words")
+def get_words(dataset_file: str):
+    app_directory = Path(__file__).resolve().parent
+    dataset_file_directory = app_directory / "data" / dataset_file
+
+    if not dataset_file_directory.is_file():
+        return {"error": "File not found"}
+
+    with dataset_file_directory.open("r", encoding="utf-8") as file:
+        data = json.load(file)
+        
+    words = data["words"]
+    
+    return {"word": [random.choice(words) for _ in range(50)]}
+    
+    
+
+
